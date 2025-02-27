86de742d0e04f44937f10b79416222cd75df992c
Jeff Puckett <jpuckett@dealerinspire.com>
2025-02-27 16:47:37 +0000

    add version label to page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# tamper-web-version
+# DI Website Version Label
+
+This [Violentmonkey](https://violentmonkey.github.io/) script displays a version label for DI Websites.
---
+++ main.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name        DI Website Version
+// @namespace   Violentmonkey Scripts
+// @match       https://*/*
+// @grant       none
+// @version     1.0
+// @author      Jeff Puckett
+// @description Shows the version of the website
+// ==/UserScript==
+const VERSION_SEARCH_NEEDLE = '"version": "';
+const VERSION_SEARCH_NEEDLE_LENGTH = 12;
+const VERSION_STRING_LENGTH = 40;
+
+function getVersion() {
+  const searchVersionPosition = document.head.innerHTML.search(
+    VERSION_SEARCH_NEEDLE
+  );
+
+  if (searchVersionPosition === -1) {
+    return "version not found";
+  }
+
+  const versionPosition = searchVersionPosition + VERSION_SEARCH_NEEDLE_LENGTH;
+
+  return document.head.innerHTML.substring(
+    versionPosition,
+    versionPosition + VERSION_STRING_LENGTH
+  );
+}
+
+function createLabel() {
+  const span = document.createElement("span");
+  span.style = "position: absolute; top: 0px; left: 0px; z-index: 100000; background-color:#fff; color: black; border: 1px solid black;";
+  span.textContent = getVersion();
+  document.body.prepend(span);
+}
+
+createLabel();
