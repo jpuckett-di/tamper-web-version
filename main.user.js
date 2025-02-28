fae5531bf900016c83947880d84e50528108a7ab
Jeff Puckett <jpuckett@dealerinspire.com>
2025-02-28 23:07:00 +0000

    add cache breaker

--- main.user.js
+++ main.user.js
@@ -11,6 +11,58 @@ const VERSION_SEARCH_NEEDLE = '"version": "';
 const VERSION_SEARCH_NEEDLE_LENGTH = 12;
 const VERSION_STRING_LENGTH = 40;
 const DEALER_INSPIRE_MAST = `\n  ______  _______ _______        _______  ______ _____ __   _ _______  _____  _____  ______ _______\n  |     \\ |______ |_____| |      |______ |_____/   |   | \\  | |______ |_____]   |   |_____/ |______\n  |_____/ |______ |     | |_____ |______ |    \\_ __|__ |  \\_| ______| |       __|__ |    \\_ |______\n         Visit http://www.dealerinspire.com to Inspire your visitors and turn them into customers.\n`;
+const CACHE_BREAKER_STORAGE_KEY = "tamper-web-version-cache-breaker";
+const CACHE_BREAKER_AUTHENTICATING = "AUTHENTICATING";
+const CACHE_BREAKER_BREAKING = "BREAKING";
+
+function authenticate() {
+  localStorage.setItem(CACHE_BREAKER_STORAGE_KEY, CACHE_BREAKER_AUTHENTICATING);
+  window.location.assign("/wp/wp-admin/");
+}
+
+function breakCache() {
+  if (
+    localStorage.getItem(CACHE_BREAKER_STORAGE_KEY) === CACHE_BREAKER_BREAKING
+  ) {
+    console.error(
+      CACHE_BREAKER_STORAGE_KEY +
+        " already attempting to break cache. aborting to prevent infinite loop"
+    );
+    return;
+  }
+
+  const url = document.querySelector(
+    'a[href*="/wp/wp-admin/admin-post.php?action=empty_cache"]'
+  )?.href;
+
+  if (!url) {
+    console.error(CACHE_BREAKER_STORAGE_KEY + " cache breaker url not found");
+    return;
+  }
+
+  localStorage.setItem(CACHE_BREAKER_STORAGE_KEY, CACHE_BREAKER_BREAKING);
+  window.location.assign(url);
+}
+
+function handleCacheBreaker() {
+  if (
+    localStorage.getItem(CACHE_BREAKER_STORAGE_KEY) ===
+    CACHE_BREAKER_AUTHENTICATING
+  ) {
+    breakCache();
+    return true;
+  }
+
+  if (
+    localStorage.getItem(CACHE_BREAKER_STORAGE_KEY) === CACHE_BREAKER_BREAKING
+  ) {
+    localStorage.removeItem(CACHE_BREAKER_STORAGE_KEY);
+    window.location.assign("/");
+    return true;
+  }
+
+  return false;
+}
 
 function isDiSite() {
   return document.childNodes[1].textContent === DEALER_INSPIRE_MAST;
@@ -45,22 +97,36 @@ function getLabelColor(version) {
   return "red";
 }
 
-function createLabel() {
+function makeVersionSpan() {
   const version = getVersion();
   const span = document.createElement("span");
   span.textContent = version;
-  span.style = `
+  span.style = `color: ${getLabelColor(version)};`;
+  return span;
+}
+
+function makeCacheBreakerButton() {
+  const button = document.createElement("button");
+  button.textContent = "break cache";
+  button.onclick = authenticate;
+  return button;
+}
+
+function createContainer() {
+  const div = document.createElement("div");
+  div.style = `
     position: absolute;
     top: 0px;
     left: 0px;
     z-index: 100000;
     background-color: white;
-    color: ${getLabelColor(version)};
     border: 1px solid black;
   `;
-  document.body.prepend(span);
+  div.appendChild(makeVersionSpan());
+  div.appendChild(makeCacheBreakerButton());
+  document.body.prepend(div);
 }
 
-if (isDiSite()) {
-  createLabel();
+if (!handleCacheBreaker() && isDiSite()) {
+  createContainer();
 }
